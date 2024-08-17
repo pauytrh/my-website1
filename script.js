5f93603b7a3ccd1915da4b842b126ccae790a9d1
pauytrh <ari.lei+github@hanisui.school>
2024-08-17 19:49:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
+document.addEventListener('DOMContentLoaded', function() {
+    const imageWrappers = document.querySelectorAll('.image-wrapper');
+
+    imageWrappers.forEach(imageWrapper => {
+        imageWrapper.addEventListener('click', function() {
+            imageWrappers.forEach(wrapper => {
+                if (wrapper !== this) {
+                    wrapper.classList.remove('expanded');
+                }
+            });
+            this.classList.toggle('expanded');
+        });
+    });
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('animate');
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
-      });
-    }, {
-      threshold: 1,
     });
-  
-    const elements = document.querySelectorAll('.head, .text, form');
-    elements.forEach((element) => {
-      observer.observe(element);
-    });
-  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
