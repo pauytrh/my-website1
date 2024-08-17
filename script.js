c8adf0589cd389b9947a78b3daf7fed40e98eb79
pauytrh <ari.lei+github@hanisui.school>
2024-08-17 11:57:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        }
+      });
+    }, {
+      threshold: 1,
+    });
+  
     const elements = document.querySelectorAll('.head, .text, form');
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate');
-            }
-        });
+    elements.forEach((element) => {
+      observer.observe(element);
     });
-
-    elements.forEach(element => {
-        observer.observe(element);
-    });
-
-    // Add click event to text elements
-    const textElements = document.querySelectorAll('.text');
-    textElements.forEach(text => {
-        text.addEventListener('click', function() {
-            text.classList.toggle('clicked');
-        });
-    });
-});
+  });
